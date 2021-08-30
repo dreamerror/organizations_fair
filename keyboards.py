1da5163421a98c2976719c0f680da0d42b941666
dreamerror <66074257+dreamerror@users.noreply.github.com>
2021-08-30 22:22:24 +0900

    first working version

--- keyboards.py
+++ keyboards.py
@@ -5,3 +5,17 @@ from aiogram import types
 
 with open('organizations.json', 'r', encoding='utf-8') as orgs:
     organizations = json.loads('\n'.join(orgs.readlines()))
+
+test_keyboards = list()
+for key in organizations.keys():
+    kb = types.InlineKeyboardMarkup()
+    for org in organizations.get(key):
+        index = organizations.get(key).index(org)
+        kb.add(types.InlineKeyboardButton(org.get('name'), callback_data=f'{"_".join(key.split(" "))}_{index}'))
+    test_keyboards.append(kb)
+
+for i in range(len(test_keyboards)):
+    if i != len(test_keyboards)-1:
+        test_keyboards[i].add(types.InlineKeyboardButton('Далее', callback_data=f'next_{i}'))
+    if i != 0:
+        test_keyboards[i].add(types.InlineKeyboardButton('Назад', callback_data=f'back_{i}'))
--- main.py
+++ main.py
@@ -7,7 +7,58 @@ from aiogram.utils import executor
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
+import keyboards as kb
 
 TOKEN = os.getenv('TOKEN')
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dp.message_handler(commands=['start'])
+async def start_message(message: types.Message):
+    await message.answer('Список организаций', reply_markup=kb.test_keyboards[0])
+    await shutdown(dp)
+
+
+@dp.callback_query_handler(lambda c: 'next' in c.data)
+async def next_page(callback_query: types.CallbackQuery):
+    index = int(callback_query.data.split('_')[-1])
+    await bot.edit_message_text(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id,
+                                text=callback_query.message.text, reply_markup=kb.test_keyboards[index + 1])
+    await shutdown(dp)
+
+
+@dp.callback_query_handler(lambda c: 'back' in c.data)
+async def prev_page(callback_query: types.CallbackQuery):
+    if len(callback_query.data.split('_')) == 1:
+        await bot.edit_message_text(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id,
+                                    text='Список организаций', reply_markup=kb.test_keyboards[0])
+    else:
+        index = int(callback_query.data.split('_')[-1])
+        await bot.edit_message_text(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id,
+                                    text=callback_query.message.text, reply_markup=kb.test_keyboards[index - 1])
+    await shutdown(dp)
+
+
+@dp.callback_query_handler(lambda c: ' '.join(c.data.split('_')[:-1]) in kb.organizations.keys())
+async def show_organisation(callback_query: types.CallbackQuery):
+    index = int(callback_query.data.split('_')[-1])
+    org = kb.organizations.get(' '.join(callback_query.data.split('_')[:-1]))[index]
+    new_kb = types.InlineKeyboardMarkup()
+    new_kb.add(types.InlineKeyboardButton('Страница организации', callback_data='org', url=org.get('url')))
+    new_kb.add(types.InlineKeyboardButton('Назад', callback_data='back'))
+    new_text = f'{org.get("name")}'
+    if org.get('description'):
+        new_text += f'\n\n{org.get("description")}'
+    await bot.edit_message_text(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id,
+                                text=new_text, reply_markup=new_kb)
+    await shutdown(dp)
+
+
+async def shutdown(dispatcher: Dispatcher):
+    await dispatcher.storage.close()
+    await dispatcher.storage.wait_closed()
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, on_shutdown=shutdown)
