4d912e609e96d301ea1a88be322096bac0a87e47
dreamerror <66074257+dreamerror@users.noreply.github.com>
2021-09-21 16:24:02 +1000

    fixed back button

--- main.py
+++ main.py
@@ -53,9 +53,10 @@ async def organizations_list(callback_query: types.CallbackQuery):
 async def go_back(callback_query: types.CallbackQuery):
     if callback_query.data != 'back':
         data = callback_query.data.replace('back_', '')
-        text = organizations.get(' '.join(kb.all_orgs.get(int(data)).split('_')))[-1].get('description')
+        text = organizations.get(' '.join(kb.all_orgs.get(data).split('_')))[-1].get('description')
+        keyboard = kb.organization_list.get(text)
         await bot.edit_message_text(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id,
-                                    text=text, reply_markup=kb.organization_list[data])
+                                    text=text, reply_markup=keyboard)
     else:
         await bot.edit_message_text(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id,
                                     text='Список направлений', reply_markup=kb.direction_list)
